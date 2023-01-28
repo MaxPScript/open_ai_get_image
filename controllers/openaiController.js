ec31cf511270b112920d042fd2a70c2ccaa2df1f
Maxim <maxtab1@mail.ru>
2023-01-28 22:23:12 +0300

    first commit

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -19,6 +19,7 @@ const generateImage = async (req, res) => {
     const imageUrl = response.data.data[0].url;
     res.status(200).json({
       success: true,
+      data: imageUrl,
     });
   } catch (error) {
     if (error.response) {
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 log = console.log;
 
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,9 @@ const app = express();
 // enable body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/openai", require("./routes/openaiRoutes"));
 
